0f2855d1c75e2db0f038de6850410b89d3f4835c
ZEE-TECH <zee-tech@ZEE-TECHs-MacBook-Pro.local>
2025-08-18 17:28:17 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// ahmed=()=>{
+//    hello = prompt("enter your age")
+
+//     if(hello<12){
+//         console.log("hello")
+//     }
+//     else{
+//         console.log("by")
+//     }
+// }
+// console.log(ahmed())
